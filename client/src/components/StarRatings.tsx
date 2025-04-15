7fbd101547fcfbf3d00297e35e6ac32405bbedc4
unknown <jerinjj202@gmail.com>
2025-04-15 13:24:22 +0530

    Changes in Client

--- client/src/components/StarRatings.tsx
+++ client/src/components/StarRatings.tsx
@@ -1,15 +1,15 @@
-import { Star } from 'lucide-react'
+import { Star } from "lucide-react";
 
-export const StarRatings = ({rating}: {rating: number}) => {
+export const StarRatings = ({ rating }: { rating: number }) => {
   return (
-    <div className='flex'>
-        {[1,2,3,4,5].map((star)=> (
-            star <= rating ? (
-                <Star key={star} className='fill-yellow-500'/>
-            ) : (
-                <Star key={star} className='fill-gray-50'/>
-            )
-        ))}
+    <div className="flex">
+      {[1, 2, 3, 4, 5].map((star) =>
+        star <= rating ? (
+          <Star key={star} className="fill-yellow-500" />
+        ) : (
+          <Star key={star} className="fill-gray-50" />
+        )
+      )}
     </div>
-  )
-}
+  );
+};
--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -1,5 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import { Pagination, PaginationContent, PaginationItem, PaginationNext, PaginationPrevious } from "@/components/ui/pagination";
+import React, { useEffect, useState } from "react";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 import {
   Table,
   TableBody,
@@ -8,87 +14,97 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table"
-import { StarRatings } from '@/components/StarRatings'
-import { Button } from '@/components/ui/button';
+} from "@/components/ui/table";
+import { StarRatings } from "@/components/StarRatings";
+import { Button } from "@/components/ui/button";
 
 interface FeedbackType {
-  name: string
-  email: string
-  phone: string
-  rating: number
-  feedback: string
+  name: string;
+  email: string;
+  phone: string;
+  rating: number;
+  feedback: string;
 }
 
 export const Admin = () => {
-
   const [page, setPage] = useState(1);
-  const [feedbacks, getFeedbacks] = useState<FeedbackType[]>([])
+  const [feedbacks, getFeedbacks] = useState<FeedbackType[]>([]);
   const [totalPages, setTotalPages] = useState(0);
-  const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('desc'); // default: newest first
+  const [sortOrder, setSortOrder] = useState<"asc" | "desc">("desc"); 
 
-const limit = 5; 
+  const limit = 5;
 
   useEffect(() => {
     const getForms = async () => {
-      const res = await fetch(`http://localhost:3001/admin?page=${page}&limit=${limit}&order=${sortOrder}`, {
-        method: 'GET',
-      })
-      const data = await res.json()
-      console.log(data)
-      getFeedbacks(data.feedbacks)
+      const res = await fetch(
+        `http://localhost:3001/admin?page=${page}&limit=${limit}&order=${sortOrder}`,
+        {
+          method: "GET",
+        }
+      );
+      const data = await res.json();
+      console.log(data);
+      getFeedbacks(data.feedbacks);
       setTotalPages(Math.ceil(data.totalCount / limit));
-    }
-    getForms()
-  }, [page, sortOrder])
+    };
+    getForms();
+  }, [page, sortOrder]);
 
   return (
     <div>
-   
-    <Table>
-      <TableCaption>Only Admin can view</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Name</TableHead>
-          <TableHead>Phone Number</TableHead>
-          <TableHead>Email</TableHead>
-          <TableHead>Ratings</TableHead>
-          <TableHead className='flex justify-between items-center my-3'>Feedback
+      <Table>
+        <TableCaption>Only Admin can view</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>Phone Number</TableHead>
+            <TableHead>Email</TableHead>
+            <TableHead>Ratings</TableHead>
+            <TableHead className="flex justify-between items-center my-3">
+              Feedback
 
-    <Button className='cursor-pointer' variant={'outline'} onClick={() => setSortOrder(sortOrder === 'desc' ? 'asc' : 'desc')}>
-    Sort by: {sortOrder === 'desc' ? 'Newest' : 'Oldest'}
-     </Button>
-          </TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {feedbacks.map((feedback: FeedbackType) => (
-          <TableRow key={feedback.name}>
-            <TableCell>{feedback.name}</TableCell>
-            <TableCell>{feedback.phone}</TableCell>
-            <TableCell>{feedback.email}</TableCell>
-            <TableCell>
-              <StarRatings key={feedback.name} rating={feedback.rating} />
-            </TableCell>
-            <TableCell>{feedback.feedback}</TableCell>
+              <Button
+                className="cursor-pointer"
+                variant={"outline"}
+                onClick={() =>
+                  setSortOrder(sortOrder === "desc" ? "asc" : "desc")
+                }
+              >
+                Sort by: {sortOrder === "desc" ? "Newest" : "Oldest"}
+              </Button>
+            </TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {feedbacks.map((feedback: FeedbackType) => (
+            <TableRow key={feedback.name}>
+              <TableCell>{feedback.name}</TableCell>
+              <TableCell>{feedback.phone}</TableCell>
+              <TableCell>{feedback.email}</TableCell>
+              <TableCell>
+                <StarRatings key={feedback.name} rating={feedback.rating} />
+              </TableCell>
+              <TableCell>{feedback.feedback}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
 
-    <Pagination className='mt-8 flex items-center'>
-  <PaginationContent>
-    <PaginationItem>
-      <PaginationPrevious onClick={() => page > 1 && setPage(page - 1)} />
-    </PaginationItem>
-    <PaginationItem>
-      Page {page} of {totalPages}
-    </PaginationItem>
-    <PaginationItem>
-      <PaginationNext onClick={() => page < totalPages && setPage(page + 1)} />
-    </PaginationItem>
-  </PaginationContent>
-</Pagination>
+      <Pagination className="mt-8 flex items-center">
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious onClick={() => page > 1 && setPage(page - 1)} />
+          </PaginationItem>
+          <PaginationItem>
+            Page {page} of {totalPages}
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext
+              onClick={() => page < totalPages && setPage(page + 1)}
+            />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
     </div>
-  )
-}
+  );
+};
